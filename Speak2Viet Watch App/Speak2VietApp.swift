d2e5bc948338a73f18ee7dd6c4f8e6f68e5c8520
realminhnguyen <real.minh.n@gmail.com>
2024-06-23 21:50:29 -0500

    Initial Commit

---
+++ Speak2Viet Watch App/Speak2VietApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speak2VietApp.swift
+//  Speak2Viet Watch App
+//
+//  Created by Minh on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Speak2Viet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Speak2Viet/Speak2VietApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speak2VietApp.swift
+//  Speak2Viet
+//
+//  Created by Minh on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Speak2VietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

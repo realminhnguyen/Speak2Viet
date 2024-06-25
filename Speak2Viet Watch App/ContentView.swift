6f3117cdea9207ff41b85cc57b502453186fc8e1
realminhnguyen <real.minh.n@gmail.com>
2024-06-24 22:47:26 -0500

    Prototyped a custom button with an SF Symbol while removing the button's default pill-shaped container.

--- Speak2Viet Watch App/ContentView.swift
+++ Speak2Viet Watch App/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Button(action: {
+                        print("Success")
+                    }) {
+                        VStack {
+                            Image(systemName: "mic.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100, height: 100)
+                                .foregroundStyle(.tint)
+                            Text("Tap to Speak")
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())  // Removes default button styling, removing the pill button container
     }
 }
 
